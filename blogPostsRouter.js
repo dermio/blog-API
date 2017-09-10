1cd64c43edfa09044bce37e1185c131db6083668
dermio <thisdjay@gmail.com>
2017-09-10 16:20:33 -0700

    added router for blog POST request

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -17,5 +17,27 @@ router.get('/', (req, res) => {
   res.json(BlogPosts.get());
 });
 
-//
+// When create new blog post, make sure required fields are passed.
+// Log error and return 400 status with message.
+// If correct, add new blog and return it with status of 201
+router.post('/', jsonParser, (req, res) => {
+  let requiredFields = ['title', 'content', 'author'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    let field = requiredFields[i];
+    if (!(field in requiredFields)) {
+      let message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  let item = BlogPosts.create(req.body.title, req.body.content,
+  req.body.author);
+  res.status(201).json(item);
+});
+
+
+
+
+//////////////
 module.exports = router;
