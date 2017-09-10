d111bc850f9940cccd956fd41744dfd987ee6ad8
dermio <thisdjay@gmail.com>
2017-09-10 14:15:56 -0700

    added code for server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+// use Express router, modularize route to /blog-posts
+// import blog-posts
+const blogPostsRouter = require('./blog-posts');
+
+
+// log http layer, copied from example shopping-list-v5
+app.use(morgan('common'));
+
+app.use(express.static('public'));
+
+/*
+app.get('/', (req, res) => {
+
+});
+*/
+//////////////
+
+
+// use app.use to route requests to /blog-posts
+
+// when requests come into `/blog-posts`,
+// we'll route them to the express
+// router instances we've imported. Remember,
+// these router instances act as modular, mini-express apps.
+app.use('/blog-posts', blogPostsRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
