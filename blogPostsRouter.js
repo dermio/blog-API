07a84b366f02be1b991ba3bd2588c869629a6849
dermio <thisdjay@gmail.com>
2017-09-10 15:12:24 -0700

    added blogPostsRouter.js

---
+++ blogPostsRouter.js
@@ -0,0 +1,18 @@
+const express = require('express');
+// create new Express router instance
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./models');
+
+// create some blog posts
+BlogPosts.create('Hello', 'Greetings from Earth', 'Davy', 'right now');
+BlogPosts.create('cake', 'I want cake', 'doggy');
+
+
+// send back JSON of blog posts on GET requests to root
+router.get('/', (req, res) => {
+  res.json(BlogPosts.get());
+});
