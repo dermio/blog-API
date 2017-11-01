e087cda3d3a6a71dd6ba252d14b7e2f0f9f994db
dermio <thisdjay@gmail.com>
2017-10-31 20:19:51 -0700

    Stubbed describe block and hook functions.

--- test/test-server.js
+++ test/test-server.js
@@ -5,3 +5,20 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 const {app, runServer, closeServer} = require("../server.js");
+
+describe("Blog Posts", function () {
+  // Call runServer() starts the server, and it also
+  // returns a Promise. This prevents the tests from running
+  // before the server is started (prevents race conditions).
+  before(function () {
+    return runServer();
+  });
+
+  // Call closeServer() after the tests run, in case other
+  // test modules need to call runServer(). If server is already running,
+  // runServer() will error out.
+  after(function () {
+    return closeServer();
+  });
+
+});
