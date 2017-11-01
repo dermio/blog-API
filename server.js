51c6a50cc365e13071a78e0c51adeb12064f3aed
dermio <thisdjay@gmail.com>
2017-10-31 17:08:15 -0700

    Added runServer function.

--- server.js
+++ server.js
@@ -29,6 +29,26 @@ app.get('/', (req, res) => {
 // these router instances act as modular, mini-express apps.
 app.use('/blog-posts', blogPostsRouter);
 
+/* old code
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
+*/
+
+
+
+// this function starts our server and returns a Promise.
+// In our test code, we need a way of asynchrnously starting
+// our server, since we'll be dealing with promises there.
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve();
+    })
+    .on("error", err => {
+      reject(err);
+    });
+  });
+}
